d73162cc7e5c57a7f84c0cf62683767e7b4c94dc
baggio4ever <baggio4ever@gmail.com>
2021-03-05 12:57:12 +0900

    crypto-ja 試してみた。いい感じだね。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-cryptojs';
+
+  onClick(){
+    const key = 'dddeeefff.';
+    const data = 'あいうえおabc憲法審査会#~=';
+
+    console.log('文字列:');
+    console.log(data);
+    console.log('===');
+
+    var ciphertext = CryptoJS.AES.encrypt(data,key).toString();
+    console.log('ciphertext:');
+    console.log(ciphertext);
+
+    console.log('---');
+    var bytes = CryptoJS.AES.decrypt(ciphertext,key);
+    var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+    console.log('元:');
+    console.log(originalText);
+  }
 }
